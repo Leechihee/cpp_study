36482d41e922811cddde43bbb3e92f664230b494
Leechihee <119794981+Leechihee@users.noreply.github.com>
2023-11-29 23:28:05 +0900

    Create 5-2 Ans.cpp

---
+++ 5-2 Ans.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <array>
+
+const int ArSize = 100;
+using namespace std;
+
+int main()
+{
+    array<long double, ArSize> factorials;
+    factorials[0] = factorials[1] = 1;
+    long double tamp = 0;
+    for(int i = 2;i<=ArSize;i++)
+    {
+        factorials[i] = factorials[i-1]*i;
+    }
+    for(int i = 0;i<=ArSize;i++)
+    {
+        tamp = factorials[i];
+    }
+    cout << tamp;
+    return 0;
+}
