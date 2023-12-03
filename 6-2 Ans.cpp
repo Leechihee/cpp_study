ea4c7ba78c102085f5ed664f5c80bfbaf3b87a25
Leechihee <119794981+Leechihee@users.noreply.github.com>
2023-12-03 19:22:08 +0900

    Create 6-2 Ans.cpp

---
+++ 6-2 Ans.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <array>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+    array<double,10> donation;
+    for(int i = 0;i<10;i++)
+    {
+        
+    }
+    return 0;
+}
