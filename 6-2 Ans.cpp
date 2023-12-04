4ce570f767ba377a743aad9d5ed8ddcd28e335e5
Leechihee <119794981+Leechihee@users.noreply.github.com>
2023-12-04 19:39:26 +0900

    Update 6-2 Ans.cpp

--- 6-2 Ans.cpp
+++ 6-2 Ans.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include <array>
-#include <cctype>
+
+const int size = 10;
 
 using namespace std;
 
 int main()
 {
-    array<double,10> donation;
-    for(int i = 0;i<10;i++)
+    cout << "기부금을 입력하시오. (숫자 외의 문자를 입력하면 프로그램이 종료)\n";
+    array<double,size> donation;
+    for(int i=1;i<size;i++)
     {
-        
+        cout << i << "번째 기부금을 입력하시오. : ";
+        if(!(cin >> donation[i]))
+            break;
+        else
+        {
+            
+        }
     }
     return 0;
 }
