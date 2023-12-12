e89a8e6365aa2bc3a5c20ac51bbb19a6b8f000b5
Leechihee <119794981+Leechihee@users.noreply.github.com>
2023-12-12 22:32:19 +0900

    Update 7-10 Ans.cpp

--- 7-10 Ans.cpp
+++ 7-10 Ans.cpp
@@ -2,7 +2,9 @@
 
 using namespace std;
 
-double calculate(double,double,double *(pf)(double,double));
+const int size = 2;
+
+double calculate(double,double,double (*pfa[])(double,double));
 
 double add(double x, double y)
 {
@@ -16,11 +18,26 @@ double add2(double x, double y)
 
 int main()
 {
+    double (*pfa[size])(double,double) = {add, add2};
+    double a,b,q;
+    cout << "입력 오류시 프로그램 종료.\n";
+    cout << "두 수를 입력하시오. (소숫점 가능) : ";
+    while(cin>> a >> b)
+    {
+        q = calculate(a,b,pfa);
+        cout << "(" << a << " + " << b << ") + (" << a << "*" << b << ") = ";
+        cout << q << endl;
+        cout << "두 수를 입력하시오. (소숫점 가능) : ";
+    }
+    cout << "입력 오류! 프로그램 종료!";
+    
     return 0;
 }
 
-double claculate(double,double,double *(pf)(double,double))
+double calculate(double a,double b ,double (*pfa[])(double,double))
 {
-    double (*pfa[2])(double,double) = {add,add2};
-    return 0;
+    double temp = 0.0;
+    for(int i = 0;i<size;i++)
+        temp += pfa[i](a,b);
+    return temp;
 }
