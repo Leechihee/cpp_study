25c45340e6b68110ef3f4a124b776e704e3fede2
Leechihee <119794981+Leechihee@users.noreply.github.com>
2023-12-23 23:41:06 +0900

    Create 10-3_golf.h

---
+++ 10-3_golf.h
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class golf{
+    static const int Len = 40;
+    struct Golf
+    {
+        char fullname[Len];
+        int handicap;
+    }GOLF;
+public:
+    golf() {GOLF.fullname[0] = '\0'; GOLF.handicap = 0;}
+    golf(const char *name, int hc);
+    golf setgolf();
+    void handicap(int hc);
+    void showgolf() const;
+};
+
