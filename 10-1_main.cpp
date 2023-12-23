e3b5f27ef5a2db8b6febb5fdf9c0ca65e361bc1f
Leechihee <119794981+Leechihee@users.noreply.github.com>
2023-12-23 22:38:00 +0900

    Create 10-1_main.cpp

---
+++ 10-1_main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "bank.h"
+
+int main()
+{
+    bank b1;
+    bank b2("lee chi hee","fgkwac", 1000);
+    b1.show();
+    b2.show();
+    b2.inMoney(2000);
+    b2.show();
+    b2.outMoney(5000);
+    b2.outMoney(1000);
+    b2.show();
+    return 0;
+}
