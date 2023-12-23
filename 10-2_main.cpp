7562574eca2b00811bef31b6cf990c5c4d4b2836
Leechihee <119794981+Leechihee@users.noreply.github.com>
2023-12-23 23:00:26 +0900

    Create 10-2_main.cpp

---
+++ 10-2_main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "person.h"
+
+namespace
+{
+    using std::cout;
+    using std::endl;
+}
+
+int main()
+{
+    person one;
+    person two("Smythecraft");
+    person three("Dimwiddy","Sam");
+    one.Show();
+    cout << endl;
+    one.FormalShow();
+    two.Show();
+    cout << endl;
+    two.FormalShow();
+    three.Show();
+    cout << endl;
+    three.FormalShow();
+    return 0;
+}
