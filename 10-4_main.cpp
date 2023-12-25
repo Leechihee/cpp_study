48dcd898b97d438158277e2c7376a5805edca9a7
Leechihee <119794981+Leechihee@users.noreply.github.com>
2023-12-25 22:32:51 +0900

    Create 10-4_main.cpp

---
+++ 10-4_main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "sales.h"
+
+int main()
+{
+    using namespace SALES;
+    double arr[] = {11.2,12.2,123.3,14.5,15.8};
+    Sales s(arr,5);
+    s.showSales();
+    s.setSales();
+    s.showSales();
+    return 0;
+}
