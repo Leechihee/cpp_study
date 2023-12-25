884592af26625ef7e85ac52d1cd57bee7953504d
Leechihee <119794981+Leechihee@users.noreply.github.com>
2023-12-25 22:33:49 +0900

    Update 10-4_sales.h

--- 10-4_sales.h
+++ 10-4_sales.h
@@ -6,33 +6,30 @@ namespace SALES
     class Sales{
         static const int QUARTERS = 4;
         double sales[QUARTERS];
-        int count = 0;
+        int count;
         double average;
-        void set_aver();
+        void set_aver()
+        {
+            double total = 0;
+            for(int i = 0;i<count;i++)
+                total+=sales[i];
+            average = total/count;
+        }
         double max;
         double min;
-        void set_max_min();
-    public:
-        Sales(const double ar[], int n);
-        Sales setSales();
-        void showSales(const Sales & s);
-    };
-    
-    void Sales::set_aver()
-    {
-        double total = 0;
-        for(int i = 0;i<count;i++)
-            total+=sales[i];
-        average = total/QUARTERS;
-    }
-    void Sales::set_max_min()
-    {
-        max = DBL_MIN;
-        min = DBL_MAX;
-        for(int i = 0;i<count;i++)
+        void set_max_min()
         {
-            max = sales[i]<max ? max:sales[i];
-            min = sales[i]<min ? sales[i]:min;
+            max = DBL_MIN;
+            min = DBL_MAX;
+            for(int i = 0;i<count;i++)
+            {
+                max = sales[i]<max ? max:sales[i];
+                min = sales[i]<min ? sales[i]:min;
+            }
         }
-    }
+    public:
+        Sales(const double ar[] = {0}, int n = 4);
+        Sales setSales();
+        void showSales();
+    };
 }
