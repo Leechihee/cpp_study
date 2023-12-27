c398415b87e48a07e74d15b9459e968574d95b62
Leechihee <119794981+Leechihee@users.noreply.github.com>
2023-12-27 22:17:04 +0900

    Create 10-7_main.cpp

---
+++ 10-7_main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "plorg.h"
+
+int main()
+{
+    Plorg p[2] = {Plorg(),Plorg("Lee Chi Hee")};
+    p[0].showPlorg();
+    p[1].showPlorg();
+    p[0].setCI(45);
+    p[1].setCI(65);
+    p[0].showPlorg();
+    p[1].showPlorg();
+    return 0;
+}
+
