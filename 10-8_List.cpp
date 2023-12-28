d46815053eb2c217acc36a43a90049032fdf9c47
Leechihee <119794981+Leechihee@users.noreply.github.com>
2023-12-28 23:51:08 +0900

    Create 10-8_List.cpp

---
+++ 10-8_List.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "List.h"
+
+namespace
+{
+    using std::cout;
+    using std::endl;
+}
+
+List::List()
+{
+    head->next = NULL;
+    cout << "리스트를 선언하였습니다.\n";
+}
+
+void List::setIndexVelue(double n)
+{
+    node * newIndex = new node;
+    node * beforeIndex;
+    newIndex->N = n;
+    newIndex->next = NULL;
+    if(head->next == NULL)
+        head->next = newIndex; // head->newIndex
+        return;
+    beforeIndex = head->next;
+    while(beforeIndex->next != NULL) 
+        beforeIndex = beforeIndex->next;
+    beforeIndex->next = newIndex;
+    cout << "벨류값을 입력하였습니다.";
+}
+
+void List::showIndex()
+{
+    node * Index = head->next;
+    /*while(Index->next != NULL)
+    {
+        cout << Index->N << endl;
+        Index = Index->next;
+    }*/
+}
+
+void List::useIndexVelue(void (*pf)(double &))
+{
+    node * beforeIndex = head->next;
+    while(beforeIndex->next != NULL)
+    {
+        pf(beforeIndex->N);
+        cout << endl;
+        beforeIndex = beforeIndex->next;
+    }
+}
