d85ef19c71df2d1fc2ec571c5aca33f08698c4fb
Leechihee <119794981+Leechihee@users.noreply.github.com>
2024-01-04 23:55:35 +0900

    Update and rename 12-3_Stack.h to 12-3_Stock.h

--- 12-3_Stock.h
+++ 12-3_Stock.h
@@ -1,18 +1,18 @@
 #include <iostream>
 
-class Stack{
+class Stock{
     char * company;
     int shares;
     double share_val;
     double total_val;
     void set_tot() {total_val = share_val * shares;}
 public:
-    Stack();
-    Stack(const char * co, long n = 0,double pr = 0.0);
-    ~Stack();
+    Stock();
+    Stock(const char * co, long n = 0,double pr = 0.0);
+    ~Stock();
     void buy(long n, double pr);
     void sell(long n, double pr);
     void update(double pr);
-    friend std::ostream & operator<<(std::ostream & os, const Stack & s);
-    const Stack & topval(const Stack & s) const;
+    friend std::ostream & operator<<(std::ostream & os, const Stock & s);
+    const Stock & topval(const Stock & s) const;
 };
