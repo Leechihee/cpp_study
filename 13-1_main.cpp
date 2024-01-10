c805cdb5d6acd04a2c0c94267594b0caff6d82bf
Leechihee <119794981+Leechihee@users.noreply.github.com>
2024-01-10 22:37:57 +0900

    Create 13-1_main.cpp

---
+++ 13-1_main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+#include "classic.h"
+void bravo(const Cd & d);
+
+int main()
+{
+    Cd c1("Beatles", "Capitol", 14, 35.5);
+    Classic c2 = Classic("Piano Sonata in B flat, Fantasia in C","Alfend Brendel", "Phillps", 2,57.17);
+    Cd *pcd = &c1;
+    
+    cout << "객체를 직접 사용한다:\n";
+    c1.report();
+    c2.report();
+    
+    cout << "객체를 지시하는 Cd * 포인터를 사용한다:\n";
+    pcd->report();
+    pcd = &c2;
+    pcd->report();
+    
+    cout << "Cd 참조 매개변수를 사용하여 함수를 호출한다:\n";
+    bravo(c1);
+    bravo(c2);
+    
+    cout << "대입을 테스트한다:\n";
+    Classic copy;
+    copy = c2;
+    copy.report();
+    return 0;
+}
+
+void bravo(const Cd & d)
+{
+    d.report();
+}
