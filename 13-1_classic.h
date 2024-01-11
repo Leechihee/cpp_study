57c9fc39cfdc1027ff37017bb19ff4dbe5d1d4cf
Leechihee <119794981+Leechihee@users.noreply.github.com>
2024-01-11 23:02:33 +0900

    Update 13-1_classic.h

--- 13-1_classic.h
+++ 13-1_classic.h
@@ -1,21 +1,23 @@
 class Cd{
-    char perforemrs[50];
+    char performers[50];
     char label[20];
     int selections;
     double playtime;
 public:
     Cd(char * s1, char * s2, int n, double x);
     Cd(const Cd & d);
+    Cd();
     virtual ~Cd();
     virtual void report() const;
     Cd & operator=(const Cd & d);
 };
 
 class Classic : public Cd{
-    char name[80];
+    char pianist[80];
 public:
     Classic(char * s1, char * s2, char * s3, int n, double x);
     Classic(const Classic & cl);
+    Classic();
     void report() const;
     Classic & operator=(const Classic & cl);
 };
