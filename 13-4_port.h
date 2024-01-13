a59af3f461bb3e5284fa6e57085cbe7d2aabe3bc
Leechihee <119794981+Leechihee@users.noreply.github.com>
2024-01-13 23:45:04 +0900

    Create 13-4_port.h

---
+++ 13-4_port.h
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Port{
+    char * brand;
+    char style[20];
+    int bottles;
+public:
+    Port(const char * br = "none", const char * st = "none", int b = 0);
+    Port(const Port & p);
+    virtual ~Port() { delete [] brand;}
+    Port & operator=(const Port & p);
+    Port & operator+=(int b);
+    Port & operator-=(int b);
+    int BottlesCount() const { return bottles; }
+    virtual void Show() const;
+    friend ostream & operator<<(ostream & os, const Port & p);
+};
+
+class VintagePort{
+    char * nickname;
+    int year;
+public:
+    VintagePort();
+    VintagePort(const char * br = "none", const char * nn = "none", int b = 0);
+    VintagePort(const VintagePort & vp);
+    ~VintagePort() { delete [] nickname; }
+    VintagePort & operator=(const VintagePort & vp);
+    void Show() const;
+    friend ostream & operator<<(ostream & os, const VintagePort & vp);
+};
