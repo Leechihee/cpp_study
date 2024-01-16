4639ffdc9fec8f0394b9b2edaefb7bf3da303c52
Leechihee <119794981+Leechihee@users.noreply.github.com>
2024-01-16 23:50:50 +0900

    Create 14-wine.cpp

---
+++ 14-wine.cpp
@@ -0,0 +1,70 @@
+#include "wine.h"
+#include <cstring>
+#include <iostream>
+
+namespace
+{
+    using namespace std;
+}
+
+Wine::Wine() : years(0)
+{
+    label = new char[strlen("NULL")+1];
+    strcpy(label,"NULL");
+    ArrayInt year;
+    ArrayInt bottles;
+    wine_infor = new PairArray(year,bottles);
+}
+
+Wine::Wine(const char * la, int y, const int yt[], const int bot[]) : years(y)
+{
+    label = new char[strlen(la)+1];
+    strcpy(label,la);
+    ArrayInt year(yt,years);
+    ArrayInt bottles(bot,years);
+    wine_infor = new PairArray(year,bottles);
+}
+
+Wine::Wine(const char * la, int y) : years(y)
+{
+    label = new char[strlen(la)+1];
+    strcpy(label,la);
+}
+
+void Wine::GetBottles()
+{
+    int in;
+    cout << years << "년간의 " << label << " 데이터를 입력하시오:\n";
+    for(int i = 0;i<years;i++)
+    {
+        int in;
+        cout << "년도를 입력하시오: ";
+        while(!(cin>>in))
+        {
+            cin.clear();
+            while(cin.get() != '\n')
+                continue;
+            cout << "숫자를 입력하시오: ";
+        }
+        wine_infor->first()[i] = in;
+        cout << "수량을 입력하시오: ";
+        while(!(cin>>in))
+        {
+            cin.clear();
+            while(cin.get() != '\n')
+                continue;
+            cout << "숫자를 입력하시오: ";
+        }
+        wine_infor->second()[i] = in;
+    }
+}
+
+void Wine::Show() const
+{
+    cout << "와인 : " << label << endl;
+    cout << "\t년도\t수량\n";
+    for(int i = 0;i<years;i++)
+        { 
+            cout << "\t"; wine_infor->first()[i]; cout << "\t"; wine_infor->second()[i]; cout << endl;
+        }
+}
