f6a34296ea55c783f0287a28f3661ad242126190
Leechihee <119794981+Leechihee@users.noreply.github.com>
2024-01-20 21:40:02 +0900

    Update 10-8_List.h

--- 10-8_List.h
+++ 10-8_List.h
@@ -1,17 +1,21 @@
+#ifdef LIST_H_
+#define LIST_H_
+
 #include <iostream>
 
-
-typedef struct Linked_List
-{
-    double N;
-    struct Linked_List * next = NULL;
-}node;
-
 class List{
+    typedef struct Linked_List
+    {
+        double N;
+        struct Linked_List * next = NULL;
+    }node;
     node * head = new node;
 public:
     List();
+    ~List();
     void setIndexVelue(double n);
     void showIndex();
     void useIndexVelue(void (*pf)(double &));
 };
+
+#endif
