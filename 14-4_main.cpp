6f469932785ba6d3e74bf550d5f0c35ac44c3e50
Leechihee <119794981+Leechihee@users.noreply.github.com>
2024-01-29 22:46:21 +0900

    Create 14-4_main.cpp

---
+++ 14-4_main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+#include "person.h"
+const int size = 10;
+void Newlinedelete();
+
+namespace
+{
+    using namespace std;
+}
+
+int main()
+{
+    Person * space[size];
+    
+    int ct;
+    for(ct = 0;ct<size;ct++)
+    {
+        char choice;
+        cout << "A. Gunslinger / B. PokerPlayer / C. BadDude / Q. Exit\n";
+        cout << "직종을 선택하시오 : ";
+        cin >> choice;
+        while(strchr("ABCQ",toupper(choice)) == NULL)
+        {
+            cout << "A, B, C 중에서 하나를 입력하시오 : ";
+            cin >> choice;
+        }
+        if(toupper(choice) == 'Q')
+        {
+            cout << "입력을 종료합니다.\n";
+            break;
+        }
+        Newlinedelete();
+        switch(toupper(choice))
+        {
+            case 'A':
+                space[ct] = new Gunslinger;
+                break;
+            case 'B':
+                space[ct] = new PokerPlayer;
+                break;
+            case 'C':
+                space[ct] = new BadDude;
+                break;
+        }
+        space[ct]->set();
+    }
+    
+    cout << "SPACE INFOR\n\n";
+    for(int i = 0;i<ct;i++)
+        {
+            space[i]->Show();
+            cout << endl;
+        }
+    for(int i = 0;i<ct;i++)
+        delete space[i];
+    return 0;
+}
+
+void Newlinedelete()
+{
+    while(cin.get() != '\n')
+        continue;
+}
