0808ecf2a8262f925c2e867924735d1c70162202
Leechihee <119794981+Leechihee@users.noreply.github.com>
2024-02-07 23:28:04 +0900

    Create 16-1_main.cpp

---
+++ 16-1_main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool palindrome(string & s);
+
+int main()
+{
+    string ans;
+    
+    cout << "단어를 입력하시오 (끝내려면 quit) : ";
+    while(getline(cin,ans) && ans != "quit")
+    {
+        if(palindrome(ans))
+            cout << "회문입니다!\n";
+        else
+            cout << "회문이 아닙니다!\n";
+        cout << "단어를 입력하시오 (끝내려면 quit) : ";
+    }
+    return 0;
+}
+
+bool palindrome(string & s)
+{
+    string temp;
+    temp.append(s.rbegin(),s.rend());
+    return s == temp;
+}
